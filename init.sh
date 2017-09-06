de557626965648c1e62dbaaa4df419bb8462cd8d
ragnarokat <31705436+ragnarokat@users.noreply.github.com>
2017-09-06 16:34:36 -0400

    Use ~/src instead of /usr/local/src

--- init.sh
+++ init.sh
@@ -18,7 +18,7 @@ then
     echo "Skipping install all. Run with: $ bash init.sh all"
 else
     echo "Installing You complete me"
-    cd /usr/local/src
+    mkdir -p ~/src && cd ~/src
     wget https://cmake.org/files/v3.8/cmake-3.8.1.tar.gz
     tar -xvzf cmake-3.8.1.tar.gz
     cd cmake-3.8.1
